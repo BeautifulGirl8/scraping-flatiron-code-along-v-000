1fd13026badd7c08077ce0f65fd246e2d1d78c26
Vicky  Tee <vttak9@gmail.com>
2021-01-29 06:09:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,14 +6,16 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))# more code coming soon!
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))# m
+  
       doc.css(".post").each do |post|
             course = Course.new
             course.title = post.css("h2").text
             course.schedule = post.css(".date").text
             course.description = post.css("p").text
-            binding.pry
+            
   end
+  binding.pry
   def print_courses
     self.make_courses
     Course.all.each do |course|
