8635fd7562376bbb4d0dbf5bb12d66a62691ecf8
Vicky  Tee <vttak9@gmail.com>
2021-01-29 06:46:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,7 @@ def make_courses
     course.schedule = post.css(".date").text
     course.description = post.css("p").text
   end
+end
   def print_courses
       self.make_courses
       Course.all.each do |course|
