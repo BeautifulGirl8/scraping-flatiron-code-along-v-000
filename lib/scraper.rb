09290b9123710fe4a6917782dc33e2c84fa96403
Vicky  Tee <vttak9@gmail.com>
2021-01-29 06:14:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,10 +13,10 @@ class Scraper
             course.title = post.css("h2").text
             course.schedule = post.css(".date").text
             course.description = post.css("p").text
-
+  binging.pry
   end
-  binding.pry
-
-
-
 end
+
+
+
+
